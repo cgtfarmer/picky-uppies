d00a7e6e2df3fa9deedd1ccb24b0c98bc589b3b7
Christian Farmer <christian.gt.farmer@gmail.com>
2023-11-04 16:56:44 -0500

    Updates

---
+++ out/_next/static/IormynkLXoYo670KUC-d3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c6623b7bb26b1e8c.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
