113941147f5f7580f853fe8318367008792fd4ea
Christian Farmer <christian.gt.farmer@gmail.com>
2023-11-06 22:03:02 -0600

    Update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
