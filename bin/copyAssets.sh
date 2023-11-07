5cd3dcfaf2982f3a41c9d64348e29ea5621bca37
Christian Farmer <christian.farmer@paymentspring.com>
2023-11-07 13:24:57 -0600

    Updates

--- bin/copyAssets.sh
+++ bin/copyAssets.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 main() {
   if [[ ! -d ./out ]]; then
     mkdir ./out
   fi
 
-  cp ./src/*.html
-  cp ./src/assets/css ./out/assets
+  cp ./src/*.html ./out
+  cp -r ./src/assets/css ./out/assets
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
