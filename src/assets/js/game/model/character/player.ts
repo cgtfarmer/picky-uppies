29aaa026c8858664fa4d187dea87cc5d2bce6e08
Christian Farmer <christian.gt.farmer@gmail.com>
2023-11-06 23:12:00 -0600

    Convert to TS setup

---
+++ src/assets/js/game/model/character/player.ts
@@ -0,0 +1,88 @@
+import { Renderable } from '../../../engine/interface/renderable';
+import Character from './character';
+import Inventory from '../inventory';
+import Sprite from '../../../engine/model/sprite/sprite';
+import Transform from '../../../engine/model/transform';
+import Vector2 from '../../../engine/model/vector2';
+import { InputModule } from '@/engine/model/input-module/input-module';
+import KeybindModule from '@/engine/model/keybind-module/keybind-module';
+
+export default class Player extends Character implements Renderable {
+
+  private inputModule: InputModule;
+
+  private keybindModule: KeybindModule | null;
+
+  private inventory: Inventory;
+
+  private velocity: Vector2;
+
+  private movementSpeed: number;
+
+  public constructor(
+    transform: Transform,
+    sprite: Sprite,
+    inputModule: InputModule,
+    name: string,
+    maxHealth: number,
+    health: number,
+    accuracy: number,
+    attackSpeed: number,
+    critChance: number,
+    critDamage: number,
+    attackRange: number,
+    inventory: Inventory
+    // autoAttackAbility: Ability,
+  ) {
+    super(
+      transform,
+      sprite,
+      name,
+      maxHealth,
+      health,
+      accuracy,
+      attackSpeed,
+      critChance,
+      critDamage,
+      attackRange,
+    );
+
+    this.keybindModule = null;
+    this.inputModule = inputModule;
+    this.inventory = inventory;
+    this.velocity = new Vector2(0, 0);
+    this.movementSpeed = 8;
+  }
+
+  public setKeybindModule(keybindModule: KeybindModule) {
+    this.keybindModule = keybindModule;
+
+    this.keybindModule.setPlayer(this);
+  }
+
+  public override update(): void {
+    // console.log('[Player#update]');
+    const horizontal: number = this.inputModule.getXAxis();
+    const vertical: number = this.inputModule.getYAxis();
+
+    this.velocity = new Vector2(
+      horizontal * this.movementSpeed,
+      (vertical * this.movementSpeed) * -1 // This is only for HTML Canvas
+    );
+
+    // console.log(`Transform before: ${this.transform.x}`);
+    // this.transform.setPosition(setX(this.transform.getX() + this.velocity.x);
+    // this.transform.setY(this.transform.getY() + this.velocity.y);
+    // this.transform.position = new Vector2(
+    //   this.transform.position.x + this.velocity.x,
+    //   this.transform.position.y + this.velocity.y,
+    // );
+    this.transform.translate(this.velocity);
+    // console.log(`Transform after: ${this.transform.x}`);
+
+    this.keybindModule?.perform();
+
+    // console.log(this.spriteRenderer);
+    this.spriteRenderer?.render();
+  }
+}
---
+++ src/assets/js/game/model/resource/resource-factory.ts
@@ -0,0 +1,107 @@
+import Transform from '../../../engine/model/transform';
+import Resource from './resource';
+import SpriteRendererFactory from '@/engine/model/sprite-renderer/sprite-renderer-factory';
+import Sprite from '@/engine/model/sprite/sprite';
+import CircleSprite from '@/engine/model/sprite/canvas/circle-sprite';
+import Vector2 from '@/engine/model/vector2';
+import Bounds from '@/engine/model/bounds';
+import Rng from '@/engine/model/rng';
+
+export default class ResourceFactory {
+
+  private static singleton: ResourceFactory;
+
+  // private readonly spriteRendererFactory: SpriteRendererFactory;
+
+  public static getInstance(): ResourceFactory {
+    if (this.singleton == null) {
+      // const spriteRendererFactory = SpriteRendererFactory.getInstance();
+      // this.singleton = new ResourceFactory(spriteRendererFactory);
+      this.singleton = new ResourceFactory();
+    }
+
+    return this.singleton;
+  }
+
+  // public constructor(spriteRendererFactory: SpriteRendererFactory) {
+  //   this.spriteRendererFactory = spriteRendererFactory;
+  // }
+
+  public createRandom(bounds: Bounds): Resource {
+    const rng: Rng = Rng.getInstance();
+
+    const roll = rng.getRandomInt(1, 3);
+
+    switch (roll) {
+      case 1:
+        return this.createPrometium(bounds.getRandomPoint());
+      case 2:
+        return this.createEndurium(bounds.getRandomPoint());
+      case 3:
+        return this.createTerbium(bounds.getRandomPoint());
+      default:
+        throw Error('Invalid roll');
+    }
+
+  }
+
+  public createPrometium(position: Vector2): Resource {
+    // const display: Display | null = Game.getInstance().getDisplay();
+
+    // if (display == null) throw Error('Display must be present');
+
+    const transform: Transform = new Transform(position);
+
+    const sprite: Sprite = new CircleSprite(transform.position, 30, true, 2, '#ff0000', '#000000');
+
+    // const spriteRenderer: SpriteRenderer =
+    //   this.spriteRendererFactory.create(sprite, display, transform);
+
+    return new Resource(
+      transform,
+      sprite,
+      'Prometium',
+      10
+    );
+  }
+
+  public createEndurium(position: Vector2): Resource {
+    // const display: Display | null = Game.getInstance().getDisplay();
+
+    // if (display == null) throw Error('Display must be present');
+
+    const transform: Transform = new Transform(position);
+
+    const sprite: Sprite = new CircleSprite(transform.position, 30, true, 2, '#0000ff', '#000000');
+
+    // const spriteRenderer: SpriteRenderer =
+    //   this.spriteRendererFactory.create(sprite, display, transform);
+
+    return new Resource(
+      transform,
+      sprite,
+      'Endurium',
+      10
+    );
+  }
+
+  public createTerbium(position: Vector2): Resource {
+    // const display: Display | null = Game.getInstance().getDisplay();
+
+    // if (display == null) throw Error('Display must be present');
+
+    const transform: Transform = new Transform(position);
+
+    const sprite: Sprite = new CircleSprite(transform.position, 30, true, 2, '#ffff00', '#000000');
+
+    // const spriteRenderer: SpriteRenderer =
+    //   this.spriteRendererFactory.create(sprite, display, transform);
+
+    return new Resource(
+      transform,
+      sprite,
+      'Endurium',
+      10
+    );
+  }
+}
