547635f8d29626f4e535faa47243da1d86d2ccac
Christian Farmer <christian.gt.farmer@gmail.com>
2023-11-18 14:43:59 -0600

    Restore application file, update index import

---
+++ site/assets/js/application.js
@@ -0,0 +1,32 @@
+import DomAccessor from './main/dom/dom-accessor.js';
+import HtmlElementFactory from './main/dom/html-element-factory.js';
+import DisplayFactory from './main/engine/model/display/display-factory.js';
+import Game from './main/engine/model/game/game.js';
+class Application {
+    static getInstance() {
+        if (this.singleton != null)
+            return this.singleton;
+        const htmlElementFactory = HtmlElementFactory.getInstance();
+        const domAccessor = DomAccessor.getInstance();
+        const displayFactory = DisplayFactory.getInstance();
+        this.singleton = new Application(htmlElementFactory, domAccessor, displayFactory);
+        return this.singleton;
+    }
+    constructor(htmlElementFactory, domAccessor, displayFactory) {
+        this.htmlElementFactory = htmlElementFactory;
+        this.domAccessor = domAccessor;
+        this.displayFactory = displayFactory;
+    }
+    start() {
+        const canvasElement = this.htmlElementFactory.createCanvas(Application.CANVAS_ID);
+        this.domAccessor.append(Application.CONTAINER_ID, canvasElement);
+        const canvasDisplay = this.displayFactory.createCanvas(canvasElement);
+        const game = Game.getInstance();
+        game.setDisplay(canvasDisplay);
+        game.start();
+    }
+}
+Application.CONTAINER_ID = 'main';
+Application.CANVAS_ID = 'canvas';
+export default Application;
+Application.getInstance().start();
