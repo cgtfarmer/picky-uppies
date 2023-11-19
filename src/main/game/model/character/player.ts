62dfc52fff0820edfacfd3e5c7f96a239388f8a9
Christian Farmer <christian.gt.farmer@gmail.com>
2023-11-18 20:31:47 -0600

    Cleanup

--- src/main/game/model/character/player.ts
+++ src/main/game/model/character/player.ts
@@ -1,14 +1,10 @@
 import { Renderable } from '../../../engine/interface/renderable';
 import Character from './character';
 import Inventory from '../inventory';
-import Sprite from '../../../engine/model/sprite/sprite';
-import Transform from '../../../engine/model/transform';
 import Vector2 from '../../../engine/model/vector2';
 import { InputModule } from '@/main/engine/model/input-module/input-module';
 import KeybindModule from '@/main/engine/model/keybind-module/keybind-module';
 import Animator from '@/main/engine/model/animator/animator';
-import Game from '@/main/engine/model/game/game';
-import RigidBody2d from '@/main/engine/model/rigid-body/rigid-body-2d';
 
 export default class Player extends Character implements Renderable {
 
@@ -32,7 +28,6 @@ export default class Player extends Character implements Renderable {
     critDamage: number,
     attackRange: number,
     inventory: Inventory
-    // autoAttackAbility: Ability,
   ) {
     super(
       animator,
@@ -66,19 +61,9 @@ export default class Player extends Character implements Renderable {
       this.inputModule.getYAxis()
     );
 
-    // rb.MovePosition(rb.position + (movement.normalized * moveSpeed * Time.fixedDeltaTime));
-
     const velocity: Vector2 = movement.normalize().multiply(this.movementSpeed);
 
     this.rigidBody?.translate(velocity);
-
-    // this.rigidBody.movePosition(this.rigidBody.getPosition() + velocity);
-
-    // this.velocity = movement.normalize()
-    //   .multiply(this.movementSpeed)
-    //   .scale(displayTransformMatrix);
-
-    // this.transform.translate(this.velocity);
 
     this.keybindModule?.perform();
 
